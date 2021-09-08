a8626a279ae9355912d92601797f71a831b412d1
You-j <ijyk3061@gmail.com>
2021-09-08 16:20:55 +0900

    add: row example

---
+++ react-style-example/src/components/dummy-box/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+export const DummbyBox = styled.div`
+  width: 100px;
+  height: 100px;
+
+  background: #5653ff;
+`;
+
+export default DummbyBox;
---
+++ react-style-example/src/components/example/row/example5.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+import React from "react";
+import DummbyBox from "../dummy-box";
+
+function Example5() {
+  <Wrapper>
+    <DummbyBox />
+    <DummbyBox />
+    <DummbyBox />
+    <DummbyBox />
+    <DummbyBox />
+  </Wrapper>;
+}
+// Frame 339 ~ 341
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-self: flex-start;
+  // align-self: center;
+  // align-self: flex-end;
+`;
